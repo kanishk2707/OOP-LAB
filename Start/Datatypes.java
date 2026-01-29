2e4b1a5345e0a540f262849fa48e45d175a53bcc
Kanishk <kanishkdhanakoti@gmail.com>
2026-01-29 14:17:56 +0530

    Add files via upload

---
+++ Start/Datatypes.java
@@ -0,0 +1,35 @@
+public class Datatypes{
+	public static void main(String[] args){
+		int a=100,a1=200;
+		double b=100.21,b1=203.21;
+		String c="hello",c1="world";
+		float d=12.4f,d1=13.2f;
+		boolean f=true,f1=false;
+		System.out.println("addition:");
+		System.out.println(a+a1);
+		System.out.println(b+b1);
+		System.out.println(c+c1);
+		System.out.println(d+d1);
+		System.out.println("subraction:");
+		System.out.println(a-a1);
+		System.out.println(b-b1);
+		System.out.println(d-d1);
+		System.out.println("multiplication:");
+		System.out.println(a*a1);
+		System.out.println(b*b1);
+		System.out.println(d*d1);
+		System.out.println("division:");
+		System.out.println(a/a1);
+		System.out.println(b/b1);
+		System.out.println(d/d1);
+		System.out.print("modulus:");
+		System.out.println(a%a1);
+		System.out.println(b%b1);
+		System.out.println(d%d1);
+		System.out.print("increment:");
+		System.out.println(a++);
+		System.out.println(b++);
+		System.out.println(d++);
+		
+	}
+}
